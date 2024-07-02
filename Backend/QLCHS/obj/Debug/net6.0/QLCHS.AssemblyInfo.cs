19a7f183211d2475701004699c5f9399dbc7c157
Trần Đình Nguyên <trandinhnguyen46159@gmail.com>
2024-07-02 14:53:51 +0700

    modify some file

--- Backend/QLCHS/obj/Debug/net6.0/QLCHS.AssemblyInfo.cs
+++ Backend/QLCHS/obj/Debug/net6.0/QLCHS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLCHS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80299fefaf69a2b976b6a4bcc035ec8f5157c2ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLCHS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLCHS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
